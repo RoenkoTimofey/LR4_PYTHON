d7d0776ed0ba5cda194b5687264dda5b523a1907
LuckyJesus150 <149528908+LuckyJesus150@users.noreply.github.com>
2023-10-31 22:27:46 +0300

    Update main.py
    змінив текст окремою функцією яка зменшила обсяг тексту, та відредагував пунктуацію.

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 text="Мировая экономика также сталкивается с вызовами, связанными с изменением климата и устойчивостью ресурсов.Стремление к сокращению выбросов парниковых газов и более эффективному использованию природных ресурсов становится всё более актуальным.Секторы, такие как здравоохранение и образование, остаются важными для развития мировой экономики.Здоровое население и образованная рабочая сила способствуют экономическому росту и инновациям.Секторы, такие как здравоохранение и образование, остаются важными для развития мировой экономики.Здоровое население и образованная рабочая сила способствуют экономическому росту и инновациям."
-count_econom=0
-for i in text.split():
-    if (i=="экономика"):
-        count_econom+=1
-print("кількість слів економіка = ",count_econom)#найти количество слов экономика 
+def reduce_text_length(text):
+    half_length = len(text) // 2
+    reduced_text = text[:half_length]
+    return reduced_text
+reduced_text = reduce_text_length(text)
+count_econom = 0
+for word in reduced_text.split():
+    if word == "экономика":
+        count_econom += 1
+print("Кількість слів 'економіка' в уменьшеному тексті =", count_econom)
+
+def add_spaces_after_period(text):
+    sentences = text.split('.')
+    sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+    result = '. '.join(sentences)
+    return result
+
+result = add_spaces_after_period(reduced_text)
+print(result)
