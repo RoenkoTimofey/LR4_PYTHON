a871c3cb6989ae47fa80095ee688a8e6ccd4837f
reeeeeeeef <149528723+reeeeeeeef@users.noreply.github.com>
2023-10-31 21:39:14 +0200

    Update2 main.py
    Результат попереднього коміту був змінений, символи були змінені на інші та також були видалені слова які складалися з більше ніж 7 елементів.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 text="Мировая экономика также сталкивается с вызовами, связанными с изменением климата и устойчивостью ресурсов.Стремление к сокращению выбросов парниковых газов и более эффективному использованию природных ресурсов становится всё более актуальным.Секторы, такие как здравоохранение и образование, остаются важными для развития мировой экономики.Здоровое население и образованная рабочая сила способствуют экономическому росту и инновациям.Секторы, такие как здравоохранение и образование, остаются важными для развития мировой экономики.Здоровое население и образованная рабочая сила способствуют экономическому росту и инновациям."
+count_econom=0
+for i in text.split():
+    if (i=="экономика"):
+        count_econom+=1
+print("кількість слів економіка = ",count_econom)#найти количество слов экономика
 def reduce_text_length(text):
     half_length = len(text) // 2
     reduced_text = text[:half_length]
@@ -18,3 +23,19 @@ def add_spaces_after_period(text):
 
 result = add_spaces_after_period(reduced_text)
 print(result)
+
+def replace_characters(sentence, old, new):
+    new_sentence = sentence.replace(old, new)
+    return new_sentence
+
+modified_text = replace_characters(result, 'о', 'x')
+print(modified_text)
+
+def remove_long_words(modified_text):
+    words = text.split()  #Разбить предложение на слова
+    filtered_words = [word for word in words if len(word) <= 7]  #Фильтровать слова
+    cleaned_text = ' '.join(filtered_words)  #Объединить слова обратно в строку
+    return cleaned_text
+
+cleaned_text = remove_long_words(modified_text)
+print(cleaned_text)
